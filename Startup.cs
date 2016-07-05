3b36771889f19eb596e60c13bf40c0f7b196daa2
VonSaeland <von_saeland@hotmail.se>
2016-07-05 13:41:36 +0200

    Add files via upload

---
+++ Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ProjektMVCCV.Startup))]
+namespace ProjektMVCCV
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}

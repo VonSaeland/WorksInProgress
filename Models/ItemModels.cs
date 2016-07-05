ecad275d7ae3b49038b780f8bf51d9f94f4b223a
Björn saeland <von_saeland@hotmail.se>
2016-07-05 14:14:35 +0200

    Add files via upload

---
+++ Models/ItemModels.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjektMVCCV.Models
+{
+    class ItemModel
+    {
+        //[Key]
+        public string Id { get; }
+        public string ItemNamne { get; set; }
+        public string Attribute { get; set; }
+    }
+}

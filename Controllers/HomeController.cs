92f8b9feb90753890514875e80a450bff294eab6
Björn saeland <von_saeland@hotmail.se>
2016-07-05 14:14:58 +0200

    Add files via upload

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ProjektMVCCV.Controllers
+{
+    [RequireHttps]
+    public class HomeController : Controller
+    {
+        ServiceReferenceItemCRUDCV.Service1Client ItemDB = new ServiceReferenceItemCRUDCV.Service1Client(); //referens till Item CRUD Servicen
+        
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Om mig";
+
+            return View();
+        }
+        public ActionResult CV()
+        {
+            ViewBag.Message = "CV";
+
+            return View();
+        }
+
+        public ActionResult Teknik()
+        {
+            ViewBag.Message = "Teknik";
+
+            return View();
+        }
+
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Björn Saeland";
+
+            return View();
+        }
+        // GET: CRUDs
+        public ActionResult GetAllItems()
+        {
+            return View(ItemDB.GetItemsList());
+        }
+
+        // GET: CRUDs/Details/5
+        public ActionResult Details(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            return View(ItemDB.GetItemById(id));
+        }
+
+        // GET: CRUDs/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+        // POST: CRUDs/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(string ItemName)
+        {
+            ItemDB.AddItem(ItemName);
+
+            return View();
+        }
+
+        // GET: CRUDs/Edit/5
+        public ActionResult Edit(string id)
+        {
+            return View(ItemDB.GetItemById(id));
+        }
+        // POST: CRUDs/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(string Id, string BookName)
+        {
+            ItemDB.UpdateItem(Id, BookName);
+            return View(ItemDB.GetItemById(Id));
+        }
+
+        // GET: CRUDs/Delete/5
+        public ActionResult Delete(string id)
+        {
+            return View(ItemDB.GetItemById(id));
+        }
+        // POST: CRUDs/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(string id)
+        {
+            ItemDB.DeleteItem(id);
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ Controllers/ItemController.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ProjektMVCCV.Controllers
+{
+    public class ItemController : Controller
+    {
+        ServiceReferenceItemCRUDCV.Service1Client ItemDB = new ServiceReferenceItemCRUDCV.Service1Client(); //referens till Item CRUD Servicen
+
+        // GET: Item
+        public ActionResult GetAllItems()
+        {
+            return View(ItemDB.GetItemsList());
+        }
+
+        // GET: Item/Details/5
+        public ActionResult Details(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            return View(ItemDB.GetItemById(id));
+        }
+
+
+        // GET: Item/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Item/Create
+        [HttpPost]
+        public ActionResult Create(string ItemName)
+        {
+            try
+            {
+                ItemDB.AddItem(ItemName);
+
+                return View();
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Item/Edit/5
+        public ActionResult Edit(string id)
+        {
+            return View(ItemDB.GetItemById(id));
+        }
+
+        // POST: Item/Edit/5
+        [HttpPost]
+        public ActionResult Edit(string Id, string ItemName)
+        {
+            try
+            {
+                ItemDB.UpdateItem(Id, ItemName);
+                return View(ItemDB.GetItemById(Id));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Item/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Item/Delete/5
+        [HttpPost]
+        public ActionResult Delete(string id)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+                ItemDB.DeleteItem(id);
+
+                return View(ItemDB.GetItemById(id));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
